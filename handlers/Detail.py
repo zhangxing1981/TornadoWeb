3325bbff51528e95877f7f43731d0c1aa96e0a21
zhangxing1981 <zhangxinglovesyou@gmail.com>
2018-04-01 10:46:01 +0800

    updated by zhangxing

--- handlers/Detail.py
+++ handlers/Detail.py
@@ -16,8 +16,10 @@ class DetailHandler(BaseHandler):
         for object in python_obj["objects"]:
             if object["objectname"] == data:
                 values["objectname"] = data
-                values["objectdescription"] = object["objectdescription"]
-
+                if object["objecttype"] == "PJ":
+                    values["objectdescription"] = "项目名称：" + object["objectfields"][0]["projectname"]  + "</br>项目背景：" + object["objectfields"][0]["background"] + "</br>项目目标：" + object["objectfields"][0]["target"]
+                else:
+                    values["objectdescription"] = object["objectdescription"]
                 break
             else:
                 values["objectname"] = "尚未创建该词条"
